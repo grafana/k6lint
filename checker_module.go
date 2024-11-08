375c9d4cee60f4056ec0d7810d89340b586689ba
Iván SZKIBA <szkiba@users.noreply.github.com>
2024-11-08 11:13:55 +0100

    feat: added build and smoke checkers

--- checker_module.go
+++ checker_module.go
@@ -2,14 +2,18 @@ package k6lint
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"regexp"
 
 	"golang.org/x/mod/modfile"
 )
 
 type moduleChecker struct {
 	file *modfile.File
+	exe  string
 }
 
 func newModuleChecker() *moduleChecker {
@@ -32,12 +36,9 @@ func (mc *moduleChecker) hasGoModule(_ context.Context, dir string) *checkResult
 	return checkPassed("found `%s` as go module", mc.file.Module.Mod.String())
 }
 
-func (mc *moduleChecker) hasNoReplace(ctx context.Context, dir string) *checkResult {
+func (mc *moduleChecker) hasNoReplace(_ context.Context, _ string) *checkResult {
 	if mc.file == nil {
-		res := mc.hasGoModule(ctx, dir)
-		if !res.passed {
-			return res
-		}
+		return checkFailed("missing go.mod")
 	}
 
 	if len(mc.file.Replace) != 0 {
@@ -45,4 +46,53 @@ func (mc *moduleChecker) hasNoReplace(ctx context.Context, dir string) *checkRes
 	}
 
 	return checkPassed("no `replace` directive in the `go.mod` file")
+}
+
+func (mc *moduleChecker) canBuild(ctx context.Context, dir string) *checkResult {
+	if mc.file == nil {
+		return checkFailed("missing go.mod")
+	}
+
+	exe, err := build(ctx, mc.file.Module.Mod.Path, dir)
+	if err != nil {
+		return checkError(err)
+	}
+
+	mc.exe = exe
+
+	return checkPassed("can be built with the latest k6 version")
+}
+
+var reSmoke = regexp.MustCompile(`(?i)^smoke(\.test)?\.(?:js|ts)`)
+
+//nolint:forbidigo
+func (mc *moduleChecker) smoke(ctx context.Context, dir string) *checkResult {
+	if mc.exe == "" {
+		return checkFailed("can't build")
+	}
+
+	filename, sortname, err := findFile(reSmoke,
+		dir,
+		filepath.Join(dir, "test"),
+		filepath.Join(dir, "tests"),
+		filepath.Join(dir, "examples"),
+	)
+	if err != nil {
+		return checkError(err)
+	}
+
+	if len(sortname) == 0 {
+		return checkFailed("no smoke test file found")
+	}
+
+	cmd := exec.CommandContext(ctx, mc.exe, "run", "--no-usage-report", "--no-summary", "--quiet", filename) //nolint:gosec
+
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, string(out))
+
+		return checkError(err)
+	}
+
+	return checkPassed("`%s` successfully run with k6", sortname)
 }
---
+++ checker_util.go
@@ -0,0 +1,102 @@
+package k6lint
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"regexp"
+	"runtime"
+
+	"github.com/grafana/k6foundry"
+)
+
+//nolint:forbidigo
+func build(ctx context.Context, module string, dir string) (filename string, result error) {
+	exe, err := os.CreateTemp("", "k6-*.exe")
+	if err != nil {
+		return "", err
+	}
+
+	if err = os.Chmod(exe.Name(), 0o700); err != nil { //nolint:gosec
+		return "", err
+	}
+
+	var out bytes.Buffer
+
+	defer func() {
+		if result != nil {
+			_, _ = io.Copy(os.Stderr, &out)
+			fmt.Fprintln(os.Stderr)
+		}
+	}()
+
+	builder, err := k6foundry.NewNativeBuilder(
+		ctx,
+		k6foundry.NativeBuilderOpts{
+			Logger: slog.New(slog.NewTextHandler(&out, &slog.HandlerOptions{Level: slog.LevelError})),
+			Stdout: &out,
+			Stderr: &out,
+			GoOpts: k6foundry.GoOpts{
+				CopyGoEnv: true,
+				Env:       map[string]string{"GOWORK": "off"},
+			},
+		},
+	)
+	if err != nil {
+		result = err
+		return "", result
+	}
+
+	result = builder.Build(
+		ctx,
+		k6foundry.NewPlatform(runtime.GOOS, runtime.GOARCH),
+		"latest",
+		[]k6foundry.Module{{Path: module, ReplacePath: dir}},
+		nil,
+		exe,
+	)
+
+	if result != nil {
+		return "", result
+	}
+
+	if err = exe.Close(); err != nil {
+		return "", err
+	}
+
+	return exe.Name(), nil
+}
+
+//nolint:forbidigo
+func findFile(rex *regexp.Regexp, dirs ...string) (string, string, error) {
+	for idx, dir := range dirs {
+		entries, err := os.ReadDir(dir)
+		if err != nil {
+			if idx == 0 {
+				return "", "", err
+			}
+
+			continue
+		}
+
+		script := ""
+
+		for _, entry := range entries {
+			if rex.Match([]byte(entry.Name())) {
+				script = entry.Name()
+
+				break
+			}
+		}
+
+		if len(script) > 0 {
+			return filepath.Join(dir, script), script, nil
+		}
+	}
+
+	return "", "", nil
+}
